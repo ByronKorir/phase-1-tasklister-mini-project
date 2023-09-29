5ad3ae52551423d0c78d8f3d346891de6283f749
byron korir <byron.korir@student.moringaschool.com>
2023-09-29 23:00:23 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ document.getElementById('create-task-form').addEventListener('submit', (e) =>{
+  e.preventDefault()
+  handleNewTask(e.target.new_task_description.value)
+ })
 });
+
+function handleNewTask(todo){
+  let li = document.createElement('li')
+  li.textContent = `${todo}   `
+   
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'done'
+  btn.style.color = 'red'
+  btn.style.backgroundColorcolor = 'green'
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function deleteTask (e){
+  e.target.parentNode.remove()
+}
